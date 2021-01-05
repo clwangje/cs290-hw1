666017de598c4b80ca768f3a9e2f2b7849b5fda2
clwangje <56001429+clwangje@users.noreply.github.com>
2021-01-05 01:54:04 -0800

    Add files via upload

---
+++ average.py
@@ -0,0 +1,13 @@
+# Author: Chenliang Wang
+# Date: 10/3/2019
+# Description: Asks the user for five numbers and then
+#              prints out the average of those numbers
+
+print("Please enter five numbers.")
+num1 = float(input())
+num2 = float(input())
+num3 = float(input())
+num4 = float(input())
+num5 = float(input())
+print("The average of those numbers is:")
+print(float((num1 + num2 + num3 + num4 + num5) / 5))
---
+++ temp_convert.py
@@ -0,0 +1,9 @@
+# Author: Chenliang Wang
+# Date: 10/3/2019
+# Description: Asks the user for temperature in Celsius and then
+#              converts and prints out the temperature in Fahrenheit
+
+print('Please enter a Celsius temperature.')
+Cel = float(input())
+print('The equivalent Fahrenheit temperature is:')
+print((9 / 5) * Cel + 32)
